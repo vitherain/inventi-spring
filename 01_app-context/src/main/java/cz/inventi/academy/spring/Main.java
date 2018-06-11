013312cb5f19390d08ab21311d80b4a508b84122
Vít Herain <vit.herain@zonky.cz>
2018-06-11 21:26:03 +0200

    Forth commit

--- 01_app-context/src/main/java/cz/inventi/academy/spring/Main.java
+++ 01_app-context/src/main/java/cz/inventi/academy/spring/Main.java
@@ -1,19 +1,18 @@
 package cz.inventi.academy.spring;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext context = new FileSystemXmlApplicationContext
+        ConfigurableApplicationContext context = new FileSystemXmlApplicationContext
                 ("src/main/resources/context.xml");
 
         Hello obj = (Hello) context.getBean("hello");
         obj.sayHello();
         obj = (Hello) context.getBean("hello");
         obj.sayHello();
-        ((FileSystemXmlApplicationContext) context).registerShutdownHook();
+        context.registerShutdownHook();
     }
 }
